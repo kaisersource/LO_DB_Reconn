83134f8f248098727addae62ae169d00b4fa1a74
kaisersource <emanuelebosimini@gmail.com>
2017-05-28 17:36:45 +0200

    minor fixes

--- con_header.hpp
+++ con_header.hpp
@@ -7,26 +7,21 @@
 #include <cppconn/statement.h>
 #include <cppconn/prepared_statement.h>
 #include <mysql_connection.h>
-std::string hostname = "kaisersource";
-std::string username = "postgres";
-std::string password = "Jobaka31";
+std::string hostname; 
+std::string username; 
+std::string password; 
 std::string db = "postgres";
-bool recon = true; // da key
 
-sql::ConnectOptionsMap propcon;
-sql::Driver *d;
-//boost::shared_ptr <sql::Connection> con; 
-//boost::shared_ptr <sql::Statement> stmt; 
-//boost:: <sql::ResultSet> res; //To grab files and 
-//boost::shared_ptr <sql::PreparedStatement> pstmt; //to avoid sql Injections
-//so these lines aren't necessary
+bool rec = true; // da key
+
+sql::mysql::MySQL_Driver *d=get_driver_instance();
+sql::ConnectOptionsMap connection_properties;
+
 sql::Connection *con;
 sql::Statement *stmt;
 sql::ResultSet  *res;
 
 
 //note:shared pointer has his own limited garbage collector, which normal pointers doesn't have. So at the end we have to delete explicitly them
-delete res;
-delete stmt;
-delete con;
+
 
--- connection.cpp
+++ connection.cpp
@@ -1,5 +1,20 @@
-driver = get_driver_instance ();    // protected    
+#include "con_header.hpp"
 
-con.reset(driver->connect (host_name, user_name, password));    // connect to mysql, passing per valor
-con->setClientOption("OPT_RECONNECT", &recon); //reconnecting      
-con->setSchema(database_name);
+    connection_properties ["OPT_CONNECT_TIMEOUT"] = 10;
+    con = d->connect(connection_properties);
+hostname="kaisersource";
+username"postgres";
+password="Jobaka31";
+
+try{    
+
+	con.reset(d->connect (hostname, username, password));    // connect to mysql, passing per value
+	con->setClientOption("OPT_RECONNECT", &rec); //true      
+	con->setSchema(database_name);
+
+}catch(Exception e){
+	//cout << "exception "<< e << '\n';
+}
+delete *res;
+delete *stmt;
+delete *con
